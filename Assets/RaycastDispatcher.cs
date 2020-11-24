60595d298327d93273c2a1e3636187ed3e413159
Saul Amster <saulamster@gmail.com>
2020-11-23 22:36:01 -0500

    got a little carried away and got reflections and directional shadows working

--- Assets/RaycastDispatcher.cs
+++ Assets/RaycastDispatcher.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 [ExecuteInEditMode, ImageEffectAllowedInSceneView]
 public class RaycastDispatcher : MonoBehaviour
 {
+    public bool useAntiAliasing = true;
     public ComputeShader rayTracingShader;
     public Texture2D skyboxTexture;
+    public Light directionalLight;
+    [Range(1, 8)]
+    public int reflections = 3;
     private RenderTexture target;
     private Camera cam;
+
+    private Material addMaterial;
+    private uint currentSample;
 
     void Awake() {
         cam = GetComponent<Camera>();
@@ -33,10 +40,31 @@ public class RaycastDispatcher : MonoBehaviour
         rayTracingShader.SetTexture(0, "SkyboxTexture", skyboxTexture); // Give shader a reference to the skybox texture
         rayTracingShader.SetMatrix("CameraToWorld", cam.cameraToWorldMatrix); // Give shader matrices for ray calculation
         rayTracingShader.SetMatrix("CameraInverseProjection", cam.projectionMatrix.inverse);
+        rayTracingShader.SetVector("SubPixelOffset", new Vector2(Random.value, Random.value));
+        rayTracingShader.SetInt("ReflectionAmount", reflections);
+
+        Vector3 l = directionalLight.transform.forward;
+        rayTracingShader.SetVector("DirectionalLight", new Vector4(l.x, l.y, l.z, directionalLight.intensity));
         
         rayTracingShader.Dispatch(0, Mathf.CeilToInt(Screen.width / 8), Mathf.CeilToInt(Screen.height / 8), 1); // Start execution of shader
 
-        //Render Output
-        Graphics.Blit(target, destination);
+        if(useAntiAliasing) {
+            if(addMaterial == null)
+                addMaterial = new Material(Shader.Find("Hidden/AddShader"));
+            addMaterial.SetFloat("_Sample", currentSample);
+            //Render Output
+            Graphics.Blit(target, destination, addMaterial);
+        } else {
+            Graphics.Blit(target, destination);
+        }
+
+
+    }
+
+    void Update() {
+        if(transform.hasChanged) {
+            currentSample = 0;
+            transform.hasChanged = false;
+        }
     }
 }
