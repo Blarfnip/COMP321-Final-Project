9011a081e97eb7e544ba28f290f43ede8f9c8ae5
Saul Amster <saulamster@gmail.com>
2020-11-23 20:50:04 -0500

    working rays and skybox. loads of comments for understanding

--- Assets/RaycastDispatcher.cs
+++ Assets/RaycastDispatcher.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
 public class RaycastDispatcher : MonoBehaviour
 {
     public ComputeShader rayTracingShader;
+    public Texture2D skyboxTexture;
     private RenderTexture target;
+    private Camera cam;
 
+    void Awake() {
+        cam = GetComponent<Camera>();
+    }
     void OnRenderImage(RenderTexture source, RenderTexture destination) {
         
         //Init render texture
@@ -23,8 +29,12 @@ public class RaycastDispatcher : MonoBehaviour
 
         //Setup and dispatch compute shader
 
-        rayTracingShader.SetTexture(0, "Result", target);
-        rayTracingShader.Dispatch(0, Mathf.CeilToInt(Screen.width / 8), Mathf.CeilToInt(Screen.height / 8), 1);
+        rayTracingShader.SetTexture(0, "Result", target); // Give shader a reference to the output texture
+        rayTracingShader.SetTexture(0, "SkyboxTexture", skyboxTexture); // Give shader a reference to the skybox texture
+        rayTracingShader.SetMatrix("CameraToWorld", cam.cameraToWorldMatrix); // Give shader matrices for ray calculation
+        rayTracingShader.SetMatrix("CameraInverseProjection", cam.projectionMatrix.inverse);
+        
+        rayTracingShader.Dispatch(0, Mathf.CeilToInt(Screen.width / 8), Mathf.CeilToInt(Screen.height / 8), 1); // Start execution of shader
 
         //Render Output
         Graphics.Blit(target, destination);
