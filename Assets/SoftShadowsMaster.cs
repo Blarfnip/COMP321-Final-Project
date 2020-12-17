f5e88a29caf460eba40d3c097ebc3e721cc460c8
pseudaimonia <57679024+pseudaimonia@users.noreply.github.com>
2020-12-17 18:19:03 -0500

    working initial tutorial work

--- Assets/SoftShadowsMaster.cs
+++ Assets/SoftShadowsMaster.cs
@@ -14,6 +14,20 @@ public class SoftShadowsMaster : MonoBehaviour
     
     private Camera camera;
     
+    private uint currentSample = 0;
+    private Material addMaterial;
+    
+    public Light DirectionalLight;
+    
+    //Range[(0.0,1.0)]
+    //public float specularity_r = 1.0;
+    
+    //Range[(0.0,1.0)]
+    //public float specularity_g = 0.78f;
+    
+    //Range[(0.0,1.0)]
+    //public float specularity_b = 0.34f
+    
     private void Awake()
     {
         camera = GetComponent<Camera>();
@@ -21,16 +35,25 @@ public class SoftShadowsMaster : MonoBehaviour
     
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        Render(destination);
+    
+        
         //Set shader parameters
         SoftShadowsShader.SetTexture(0,"SkyboxTexture", SkyboxTexture);
         SoftShadowsShader.SetMatrix("CameraToWorld", camera.cameraToWorldMatrix);
         SoftShadowsShader.SetMatrix("CameraInverseProjection", camera.projectionMatrix.inverse);
-        
+        SoftShadowsShader.SetVector("PixelOffset", new Vector2(Random.value, Random.value));
+        Vector3 l = DirectionalLight.transform.forward;
+        SoftShadowsShader.SetVector("DirectionalLight", new Vector4(l.x, l.y, l.z, DirectionalLight.intensity));
+        //SoftShadowsShader.SetFloat("specularity_r", specularity_r);
+        //SoftShadowsShader.SetFloat("specularity_g", specularity_g);
+        //SoftShadowsShader.SetFloat("specularity_b", specularity_b);
+
+        Render(destination);
     }
     
     private void Render(RenderTexture destination)
     {
+        
         //initializes render texture
         if(target == null || target.width != Screen.width || target.height!=Screen.height)
         {
@@ -41,9 +64,10 @@ public class SoftShadowsMaster : MonoBehaviour
                 RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
             target.enableRandomWrite = true;
             target.Create();
+            currentSample = 0;
         }
         
-        // target texture initialized
+        // initialization finished
         
         
         SoftShadowsShader.SetTexture(0,"Result",target);
@@ -51,12 +75,29 @@ public class SoftShadowsMaster : MonoBehaviour
         int threadGroupsy = Mathf.CeilToInt(Screen.height/8.0f);
         SoftShadowsShader.Dispatch(0,threadGroupsX,threadGroupsy,1);
         
-        Graphics.Blit(target, destination);
+        // Blit the result texture to the screen
+        if (addMaterial == null)
+            addMaterial = new Material(Shader.Find("Hidden/ExtraShader"));
+        addMaterial.SetFloat("_Sample", currentSample);
+        Graphics.Blit(target, destination, addMaterial);
+        currentSample++;
     }
     
     // Update is called once per frame
     void Update()
     {
+    
+        if (transform.hasChanged)
+        {
+            currentSample = 0;
+            transform.hasChanged = false;
+        }
+        if(DirectionalLight.transform.hasChanged)
+        {
+            currentSample = 0;
+            transform.hasChanged = false;
+        
+        }
         
     }
 }
